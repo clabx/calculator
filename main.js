3dbbd6aedb25f9d7a44f78d236fba65b434b6a0d
mcal-mc <dev@mcalvo.ar>
2023-01-07 20:14:21 -0300

    Improve calculator's functionality and make display functional
    Make various calculations possible in one go, add the posibility to operate with decimal point and show results and equations on display.

--- main.js
+++ main.js
@@ -25,11 +25,15 @@ function convertNumber(string){
 }
 
 function resetData(){
-    firstNumber = "";
-    secondNumber = "";
-    currentNumber = "";
-    operator = "";
-    result = "";
+firstNumber = "";
+currentNumber = "";
+secondNumber = "";
+operator = "";
+oldOperator = "";
+result = "";
+isDecimal = false;
+firstNumberToShow = "";
+secondNumberToShow = "";
 }
 
 function calculate(numberA, numberB, op){
@@ -45,12 +49,28 @@ function calculate(numberA, numberB, op){
     }
 }
 
+function whichOperator(operator){
+    if(operator === 'plus'){
+        return '+';
+    } else if(operator === 'minus'){
+        return '-';
+    } else if(operator === 'multiply'){
+        return '×';
+    } else if(operator === 'division'){
+        return '/';
+    }
+}
+
 let firstNumber = "";
 let currentNumber = "";
 let secondNumber = "";
 let operator = "";
 let oldOperator = "";
 let result = "";
+let isDecimal = false;
+let firstNumberToShow = "";
+let secondNumberToShow = "";
+let resetRequest = false;
 
 // Query selectors and listeners.
 
@@ -58,7 +78,6 @@ const numbersDOM = document.querySelectorAll(".number");
 numbersDOM.forEach((button) => {
     button.addEventListener('click', (e) => {
         currentNumber += convertNumber(e.path[0].id);
-        console.log(currentNumber); 
     })
 })
 
@@ -72,13 +91,23 @@ operandsDOM.forEach((button) => {
         if(firstNumber === '' && secondNumber === ''){
             firstNumber = currentNumber;
             currentNumber = '';
+            isDecimal = false
         } else{
             secondNumber = currentNumber;
             firstNumber = calculate(firstNumber, secondNumber, oldOperator);
             secondNumber = '';
             currentNumber = '';
+            isDecimal = false
             }
     })
+})
+
+const decimalDOM = document.querySelector("#decimal");
+decimalDOM.addEventListener('click', () => {
+    if(!isDecimal && currentNumber != ''){
+        currentNumber += '.';
+        isDecimal = true;
+    }
 })
 
 const equalDOM = document.querySelector('#equal');
@@ -86,10 +115,39 @@ equalDOM.addEventListener('click', () => {
     secondNumber = currentNumber;
     if(firstNumber != '' && secondNumber != ''){
         result = calculate(firstNumber, secondNumber, operator);
-        firstNumber = result;
-        secondNumber = '';
-        operator = '';
-        console.log(result)
     }
 })
 
+const displayEquationDOM = document.querySelector('#displayEquation');
+const displayResultDOM = document.querySelector('#displayResult');
+const buttonsDOM = document.querySelectorAll('button');
+buttonsDOM.forEach((button) => {
+    button.addEventListener('click', () => {
+        let equationText;
+        if(firstNumber === ''){
+            equationText = currentNumber;
+        } else {
+            equationText = firstNumber;
+            if(operator != ''){
+                equationText += " " + whichOperator(operator);
+                if(currentNumber != ''){
+                    equationText += " " + currentNumber;
+                }
+            }
+        }
+        displayEquationDOM.textContent = equationText;
+
+            if(result !== ''){
+                displayResultDOM.textContent = parseFloat(result.toPrecision(4));
+                resetData();
+                displayEquationDOM.textContent = '';
+            }
+    })    
+})
+
+const clearDOM = document.querySelector("#clear");
+clearDOM.addEventListener('click', () => {
+    resetData();
+    displayResultDOM.textContent = '';
+    displayEquationDOM.textContent = '';
+})
